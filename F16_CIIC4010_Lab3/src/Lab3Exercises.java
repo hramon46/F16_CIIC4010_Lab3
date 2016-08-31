93fadb60206d7ed801086a61a53d7857e45228ab
4010c2 <4010c2@chironja.ece.uprm.edu>
2016-08-31 12:23:29 -0400

    CHANGES
    Changes were made to the original file to where the size, location and
    JFrame title were modified.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hector Collazo");
+		JFrame myFrame = new JFrame("Hector Collazo Melendez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(1024, 576);
 		myFrame.setSize(200, 200);
